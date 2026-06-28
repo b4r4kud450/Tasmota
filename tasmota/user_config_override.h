13ba905cba830ee429e64976fb63b9491f36faaa
b4r4kud450 <b4r4kud@gmail.com>
2026-06-28 18:28:14 +0300

    Add files via upload

--- tasmota/user_config_override.h
+++ tasmota/user_config_override.h
@@ -1,33 +1,30 @@
 /*
   user_config_override.h - Tasmota для Freenove ESP32-S3 WROOM + OV3660
-  Скопіюйте в папку tasmota/ як user_config_override.h
 */
 
 #ifndef _USER_CONFIG_OVERRIDE_H_
 #define _USER_CONFIG_OVERRIDE_H_
 
-// -- Webcam налаштування --------------------------
-#undef  USE_WEBCAM
-#define USE_WEBCAM                         // Увімкнути підтримку вебкамери
-//#define USE_WEBCAM_V2                      // Використати новий драйвер V2 (кращий для ESP32-S3)
+#define USE_WEBCAM
+#define USE_ESP32_CAM
 
-// -- Ініціалізація камери при старті --------------
-// Команда яка виконається при завантаженні:
-// WcInit - ініціалізує камеру
-// WcResolution 8 - встановлює VGA (640x480)
+#define PWDN_GPIO_NUM     -1
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM     15
+#define SIOD_GPIO_NUM     17
+#define SIOC_GPIO_NUM     18
+#define Y9_GPIO_NUM       39
+#define Y8_GPIO_NUM       40
+#define Y7_GPIO_NUM       41
+#define Y6_GPIO_NUM       42
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       48
+#define Y3_GPIO_NUM       47
+#define Y2_GPIO_NUM       14
+#define VSYNC_GPIO_NUM    38
+#define HREF_GPIO_NUM     45
+#define PCLK_GPIO_NUM     16
 
-// -- WiFi (за бажанням, можна налаштувати через вебінтерфейс)
-//#undef  STA_SSID1
-//#define STA_SSID1     "your_wifi_name"
-//#undef  STA_PASS1
-//#define STA_PASS1     "your_wifi_password"
-
-// -- Hostname
-#undef  WIFI_HOSTNAME
 #define WIFI_DEFAULT_HOSTNAME "freenove-cam"
 
-// -- MQTT (за бажанням)
-//#undef  MQTT_HOST
-//#define MQTT_HOST     "192.168.1.100"
-
-#endif  // _USER_CONFIG_OVERRIDE_H_
+#endif
